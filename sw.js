7110142d2c782ab2f5c7860e0c600d124c782a63
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-05-08 13:12:27 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
 // This is the "Offline copy of pages" service worker
-
+Notification.requestPermission();
 const CACHE = "pwabuilder-offline";
+
+
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
@@ -10,9 +12,57 @@ self.addEventListener("message", (event) => {
   }
 });
 
+// Check to make sure Sync is supported.
+if ('serviceWorker' in navigator && 'SyncManager' in window) {
+
+  // Get our service worker registration.
+  const registration = await navigator.serviceWorker.registration;
+
+  try {
+    // This is where we request our sync. 
+    // We give it a "tag" to allow for differing sync behavior.
+    await registration.sync.register('database-sync');
+
+  } catch {
+    console.log("Background Sync failed.")
+  }
+}
+
+
+// Add an event listener for the `sync` event in your service worker.
+self.addEventListener('sync', event => {
+
+  // Check for correct tag on the sync event.
+  if (event.tag === 'database-sync') {
+
+    // Execute the desired behavior with waitUntil().
+    event.waitUntil(
+
+      // This is just a hypothetical function for the behavior we desire.
+      pushLocalDataToDatabase();
+    );
+    }
+});
+
+
 workbox.routing.registerRoute(
   new RegExp('/*'),
   new workbox.strategies.StaleWhileRevalidate({
     cacheName: CACHE
   })
 );
+
+self.addEventListener('push', (event) => {
+  event.waitUntil(
+    self.registration.showNotification('Notification Title', {
+      body: 'Notification Body Text',
+      icon: '/assets/favicon-1248x1248.png',
+    });
+  );
+});
+
+self.addEventListener('notificationclick', (event) => {
+    event.notification.close(); 
+    var fullPath = self.location.origin + event.notification.data.path; 
+    clients.openWindow(fullPath); 
+});
